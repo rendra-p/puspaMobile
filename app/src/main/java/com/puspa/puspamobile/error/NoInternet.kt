7e70f0fc3a32eec7d7b685627dd28267425a3830
rendra-p <rendraprasetya32@gmail.com>
2025-10-02 17:30:40 +0700

    Initial commit

---
+++ app/src/main/java/com/puspa/puspamobile/error/NoInternet.kt
@@ -0,0 +1,21 @@
+package com.puspa.puspamobile.error
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.puspa.puspamobile.R
+
+class NoInternet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_no_internet)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
