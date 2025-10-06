c612422fcb717b9fead5f7f4afa1be80a8b75bc0
rendra-p <rendraprasetya32@gmail.com>
2025-10-06 19:49:28 +0700

    Build UI Register

---
+++ app/src/main/java/com/puspa/puspamobile/ui/auth/RegisterActivity.kt
@@ -0,0 +1,19 @@
+package com.puspa.puspamobile.ui.auth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.puspa.puspamobile.R
+import com.puspa.puspamobile.databinding.ActivityLoginBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
