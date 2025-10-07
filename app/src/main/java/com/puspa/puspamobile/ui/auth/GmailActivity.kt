8041dd339795383f6d7791da4dc3d9951593ebe1
rendra-p <rendraprasetya32@gmail.com>
2025-10-07 17:40:16 +0700

    Set Button Logic GmailActivity.kt

--- app/src/main/java/com/puspa/puspamobile/ui/auth/GmailActivity.kt
+++ app/src/main/java/com/puspa/puspamobile/ui/auth/GmailActivity.kt
@@ -1,6 +1,9 @@
 package com.puspa.puspamobile.ui.auth
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,5 +18,36 @@ class GmailActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityGmailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.imgbtnBack.setOnClickListener {
+            finish()
+        }
+        binding.btnOpenEmail.setOnClickListener {
+            openEmailApp()
+        }
+    }
+
+    private fun openEmailApp() {
+        val intent = Intent(Intent.CATEGORY_APP_EMAIL).apply {
+            addCategory(Intent.CATEGORY_APP_EMAIL)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            tryOpenAlternative()
+        }
+    }
+
+    private fun tryOpenAlternative() {
+        val alternativeIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+        }
+
+        if (alternativeIntent.resolveActivity(packageManager) != null) {
+            startActivity(alternativeIntent)
+        } else {
+            Toast.makeText(this, "Tidak ada aplikasi email yang terinstal.", Toast.LENGTH_LONG).show()
+        }
     }
 }
