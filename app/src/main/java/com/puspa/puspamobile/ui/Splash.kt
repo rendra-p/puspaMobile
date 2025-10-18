7e38930d5086cc0efe4bc742b9bf6e4ade65c720
rendra-p <rendraprasetya32@gmail.com>
2025-10-18 20:43:00 +0700

    Adding Progression Bar at Splash

--- app/src/main/java/com/puspa/puspamobile/ui/Splash.kt
+++ app/src/main/java/com/puspa/puspamobile/ui/Splash.kt
@@ -1,9 +1,11 @@
 package com.puspa.puspamobile.ui
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.LinearInterpolator
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.puspa.puspamobile.databinding.ActivitySplashBinding
@@ -13,12 +15,18 @@ import com.puspa.puspamobile.ui.error.NoInternet
 
 class Splash : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
+    private val SPLASH_TIME_OUT: Long = 3000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val progressAnimation = ObjectAnimator.ofInt(binding.progressBar, "progress", 0, 100)
+        progressAnimation.duration = SPLASH_TIME_OUT
+        progressAnimation.interpolator = LinearInterpolator()
+        progressAnimation.start()
 
         Handler(Looper.getMainLooper()).postDelayed({
             if (NetworkUtils.isInternetAvailable(this)) {
@@ -28,6 +36,6 @@ class Splash : AppCompatActivity() {
                 startActivity(Intent(this, NoInternet::class.java))
                 finish()
             }
-        }, 3000)
+        }, SPLASH_TIME_OUT)
     }
 }
